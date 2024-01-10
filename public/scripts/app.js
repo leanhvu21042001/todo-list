b2da605b28f01efc995a51b85dcb8f19c4088022
Trần Phát Đạt <datbandat123456@gmail.com>
2024-01-10 09:30:48 +0700

    feat: add function delete todo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,23 @@
 //add Todo
 let todoList = [];
 
-const formAdd = document.querySelector('#formAdd');
-formAdd.addEventListener('submit', function (event) {
+const formAdd = document.querySelector("#formAdd");
+formAdd.addEventListener("submit", function (event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const todoText = formData.get('todoText');
-  todoList = [...todoList, {
-    id: Date.now(),
-    context: todoText,
-    mark: false,
-    created: Date.now().toString(),
-  }]
-
+  const todoText = formData.get("todoText");
+  todoList = [
+    ...todoList,
+    {
+      id: Date.now(),
+      context: todoText,
+      mark: false,
+      created: Date.now().toString(),
+    },
+  ];
 });
+
+//remove Todo
+function removeTodo(id, todos = []) {
+  return todos.filter((todo) => Number(todo.id) !== Number(id));
+}
