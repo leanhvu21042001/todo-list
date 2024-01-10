07472b712f50a76d6f437e240de0d655aabe65ba
Trần Phát Đạt <106939172+DatEoW@users.noreply.github.com>
2024-01-10 08:14:27 +0700

    Feat/add todo (#3)
    * ui:formadd-todo
    
    * feat: addTodo
    
    ---------
    
    Co-authored-by: Vu Le Anh <vula.35.student@fit.tdc.edu.vn>

---
+++ public/scripts/app.js
@@ -0,0 +1,16 @@
+//add Todo
+let todoList = [];
+
+const formAdd = document.querySelector('#formAdd');
+formAdd.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const todoText = formData.get('todoText');
+  todoList = [...todoList, {
+    id: Date.now(),
+    context: todoText,
+    mark: false,
+    created: Date.now().toString(),
+  }]
+  
+});
