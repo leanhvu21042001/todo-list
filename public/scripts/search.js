155f681e31b9a870ec87155d72c053f6dfa60629
Vu Le Anh <vula.35.student@fit.tdc.edu.vn>
2024-01-10 14:30:25 +0700

    feat: clear search

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -8,24 +8,35 @@ document
     const created = formData.get("searchCreated");
     const status = formData.get("searchStatus");
 
-    const todoFound = todoList.filter((obj) => obj.content.includes(term));
+    let todoFound = todoList.filter((obj) => obj.content.includes(term));
 
     if (status) {
-      todoFound.filter((todo) => todo.isMarked === Number(status));
+      todoFound = todoFound.filter(
+        (todo) => Number(todo.isMarked) === Number(status)
+      );
     }
 
     if (created && created === "asc") {
       todoFound.sort(function (a, b) {
-        return a.createdAt - b.createdAt;
+        return Number(a.createdAt) - Number(b.createdAt);
       });
     }
 
     if (created && created === "desc") {
       todoFound.sort(function (a, b) {
-        return b.createdAt - a.createdAt;
+        return Number(b.createdAt) - Number(a.createdAt);
       });
     }
 
     const elements = createTodoElements(todoFound);
     renderTodoElements(elements);
   });
+
+document.querySelector("#resetButton").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  document.querySelector("#form-search").reset();
+
+  const elements = createTodoElements(todoList);
+  renderTodoElements(elements);
+});
