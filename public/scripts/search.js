0aeee9cb6b9021d117cc0294071e447a29395432
Trần Phát Đạt <106939172+DatEoW@users.noreply.github.com>
2024-01-10 14:14:39 +0700

    feat:search-todo (#9)

---
+++ public/scripts/search.js
@@ -0,0 +1,31 @@
+document
+  .querySelector("#form-search")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const term = formData.get("searchTerm");
+    const created = formData.get("searchCreated");
+    const status = formData.get("searchStatus");
+
+    const todoFound = todoList.filter((obj) => obj.content.includes(term));
+
+    if (status) {
+      todoFound.filter((todo) => todo.isMarked === status);
+    }
+
+    if (created && created === "asc") {
+      todoFound.sort(function (a, b) {
+        return a.createdAt - b.createdAt;
+      });
+    }
+
+    if (created && created === "desc") {
+      todoFound.sort(function (a, b) {
+        return b.createdAt - a.createdAt;
+      });
+    }
+
+    const elements = createTodoElements(todoFound);
+    renderTodoElements(elements);
+  });
